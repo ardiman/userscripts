722cd4075b0289fbeac8a7db235b6148967ba5d6
ardiman <sobotnik@gmx.net>
2014-11-09 17:34:18 +0100

    Script - first submit

---
+++ mediawikilinktoedituser/mediawikilinktoedituser.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         MediaWiki LinkToEditUser
+// @namespace    http://openuserjs.org/users/ardiman
+// @description  Gives on userpages of mediawikis a link to Special:EditUser (you'll need mw-extension EditUser on that wiki).
+// @grant        none
+// @homepage     https://github.com/ardiman/userscripts/tree/master/mediawikilinktoedituser
+// @include      /wiki/(Benutzer|User).*/
+// @include      /title=(Benutzer|User):.*&action=edit/
+// @version      1.0.1
+// @date         2013-06-16
+// ==/UserScript==
+
+var h1First = document.evaluate("//h1[@id='firstHeading']//span", document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE , null);
+var userName = h1First.snapshotItem(0).innerHTML.replace(/ /g,"+").replace(/“/g,"").replace(/\/.*/g,"");
+userName = encodeURI(userName.substring(userName.lastIndexOf(":")+1));
+
+var ulMenu = document.evaluate("//div[@id='p-cactions']//div//ul", document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE , null);
+var liChConf = ulMenu.snapshotItem(0).appendChild(document.createElement('li'));
+
+liChConf.innerHTML='<a href="/index.php?title=Special:EditUser&amp;username='+userName+'">Konfiguration</a>';

2180ffb6f3bf58f1468f429aee0a43197c367df3
ardiman <sobotnik@gmx.net>
2014-11-10 20:10:50 +0100

    Script - logo included

--- fxforumnewtab/fxforumnewtab.user.js
+++ fxforumnewtab/fxforumnewtab.user.js
@@ -5,9 +5,10 @@
 // @description:de-DE Öffnet Links in Beiträgen im neuen Tab.
 // @grant        none
 // @homepage     https://github.com/ardiman/userscripts/tree/master/fxforumnewtab
+// @icon         https://raw.githubusercontent.com/ardiman/userscripts/master/scriptlogo.gif
 // @include      http://www.camp-firefox.de/forum/*
-// @version      1.0.3
-// @date         2014-11-08
+// @version      1.0.4
+// @date         2014-11-10
 // ==/UserScript==
 
 var res = document.evaluate("//a[@class='postlink-local']|//a[@class='postlink']", document, null,

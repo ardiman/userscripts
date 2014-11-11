d52629c3f14764546ca5b9054acb0d91d35455b5
ardiman <sobotnik@gmx.net>
2014-11-11 20:57:11 +0100

    Script - typo in description

--- xenforoselectpermalink/xenforoselectpermalink.user.js
+++ xenforoselectpermalink/xenforoselectpermalink.user.js
@@ -2,14 +2,14 @@
 // @name         xenForo - Select Permalink
 // @namespace    http://openuserjs.org/users/ardiman
 // @description  Selects whole permalink-code in xenForo-boards with one click.
-// @description:de-DE Markiert in xenForo-Foren den gesamten Permalink-Code mit einem einfachen Klick aus.
+// @description:de-DE Markiert in xenForo-Foren den gesamten Permalink-Code mit einem einfachen Klick.
 // @grant        none
 // @homepage     https://github.com/ardiman/userscripts/tree/master/xenforoselectpermalink
 // @icon         https://raw.githubusercontent.com/ardiman/userscripts/master/scriptlogo.gif
 // @include      http://xenforo.com/community/threads/*
 // @include      http://www.xendach.de/threads/*
-// @version      1.0.3
-// @date         2014-11-10
+// @version      1.0.4
+// @date         2014-11-11
 // ==/UserScript==
 
 $( document ).on( "click", "#bb_code_link_snippet,#html_link_code", function() {
